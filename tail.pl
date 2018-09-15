943ece9c8abf0c2d28a9a5582da4775141a6f714
Andrew Taylor <andrewt@unsw.edu.au>
2018-09-16 00:19:21 +1000

    Friend's attempt at tail.pl

--- tail.pl
+++ tail.pl
@@ -11,10 +11,21 @@ if (@ARGV >= 0 && $ARGV[0] =~ /-([0-9]+)/) {
 # If there are no files to print, just print standard input
 if (@ARGV == 0) {
     @lines = <INPUT>;
+    $first = 0;
+    $first = @lines - $max_lines unless @lines < $max_lines;
+    print @lines[$first..$#lines];
 
 # otherwise tail all given files,
 # interposing with ==> filename <== if more than 1 file
 } else {
+    $show_file_names = @ARGV > 1;
     foreach $file (@ARGV) {
+        open my $f, '<', $file or die "$0: can't open $file\n";
+        print "==> $file <==\n" unless $show_file_names;
+        @lines = <$f>;
+        $first = 0;
+        $first = @lines - $max_lines unless @lines < $max_lines;
+        print @lines[$first..$#lines];
+        close $f;
     }
 }
